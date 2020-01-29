b62e70ea9eadd5aa9345394e54641c1a6c8084fd
StaarX <56207081+StaarX@users.noreply.github.com>
2020-01-29 14:29:34 -0700

    Project Commit
    Initial project commit

---
+++ Controllers/AppController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DutchTreat.ViewModels;
+using DutchTreat.Services;
+
+namespace DutchTreat.Controllers
+{
+    public class AppController : Controller
+    {
+        private readonly IMailService mailservice;
+
+        public AppController(IMailService mailservice)
+        {
+            this.mailservice = mailservice;
+        }
+        public IActionResult Index()
+        {
+            //throw new InvalidOperationException();
+            return View();
+        }
+        [HttpGet("contact")]
+        public IActionResult Contact()
+        {
+            ViewBag.Title = "Contact Us";
+
+            return View();
+        }
+        [HttpPost("contact")]
+        public IActionResult Contact(ContactViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //Send the email
+                this.mailservice.SendMessage("shawn@wildermuth.com", model.Subject, $"From : {model.Name},{model.Email}, Message: {model.Message} ");
+                ViewBag.UserMessage = "Mail Sent";
+                ModelState.Clear();
+            }
+            else
+            {
+                //Show the error
+            }
+
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewBag.Title = "About Us";
+            return View();
+        }
+
+    }
+}
---
+++ wwwroot/js/index.js
@@ -0,0 +1,30 @@
+﻿$(document).ready(function () {
+    console.log("Hello pluralsight");
+
+    var thForm = $("#theForm");
+    thForm.hide();
+
+
+
+    var button = $("#buyButton");
+    button.on("click", () => {
+        console.log("Buying item...");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", () => {
+        console.log(`You have clicked on ${$(this).text()}`);
+    });
+
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", () => {
+        $popupForm.slideToggle(200);
+    });
+
+
+
+
+});
